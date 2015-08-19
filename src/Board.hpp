d0553f7c6b31e270402f84ccf1f5824a35a979f2
Saul <frmsaul@gmail.com>
2015-08-19 00:08:25 -0700

    Added the bool is_move_allowed() functionality

--- src/Board.hpp
+++ src/Board.hpp
@@ -13,10 +13,12 @@ public:
   float eval() const;
   void print(Player player) const;
   
-  std::vector<Board> get_child_states();
+  std::vector<Board> get_child_states() const;
   
+  bool is_player_allowed_to_move_piece(const int& row,const int& col) const;
+  bool is_move_allowed(Move m, int row, int col) const;
+  Board make_move(Move m) const;
 private:
-  Board make_move(Move m);
   Piece board_[10][10];
   Player player_;
 };
