449697dfaed4d2407c4bde9f4920ac50ae6ddc04
Saul <frmsaul@gmail.com>
2015-08-20 22:18:56 -0700

    Added BoardIO files

---
+++ src/BoardIO.cpp
@@ -0,0 +1,12 @@
+#include "BoardIO.hpp"
+
+#include <string>
+#include <iostream>
+
+#include <stdio.h>
+
+#include "Board.hpp"
+#include "Piece.hpp"
+
+
+
---
+++ src/BoardIO.hpp
@@ -0,0 +1,13 @@
+#ifndef _H_BOARD_IO
+#define _H_BOARD_IO
+
+#include <string>
+
+void print_board(const Board& b);
+void write_board_to_file(std::string file_name, const Board& b);
+void write_board_web_view(std::string file_name, const Board& b);
+
+Board read_board_from_file(std::string file_name);
+
+
+#endif
