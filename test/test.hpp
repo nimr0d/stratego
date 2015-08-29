a8abe05388877565693b8b88f20a527b8df1b398
Nimrod Hajaj <hrodenticus@gmail.com>
2015-08-28 21:10:49 -0700

    Removed BoardIO include in test

--- test/test.hpp
+++ test/test.hpp
@@ -3,7 +3,6 @@
 
 #include "test_piece.hpp"
 #include "test_Board.hpp"
-#include "test_BoardIO.hpp"
 
 #include <stdlib.h>
 #include <time.h>
@@ -13,7 +12,6 @@ void test(){
   srand(time(NULL));
   test_piece();
   test_Board();
-  test_BoardIO();
 }
 
 #endif 
